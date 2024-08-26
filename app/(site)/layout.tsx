deea5dfe11763e29db65ade669e978fdaf04d980
Mahmoud Abdullah Anani <99493135+MahmoudAbdullahAnani@users.noreply.github.com>
2024-08-26 12:11:38 +0300

    add google analytics

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -14,11 +14,53 @@ const inter = Inter({ subsets: ["latin"] });
 
 import ToasterContext from "../context/ToastContext";
 
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import Script from "next/script";
+const GA_TRACKING_ID = "G-SPNF9L10Q9";
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  useEffect(() => {
+    // Check if window is available
+    if (typeof window !== "undefined") {
+      // Initialize Google Analytics
+      // @ts-ignore
+      window.dataLayer = window.dataLayer || [];
+
+      // @ts-ignore
+      function gtag() {
+        // @ts-ignore
+        window.dataLayer.push(arguments);
+      }
+      // @ts-ignore
+      gtag("js", new Date());
+      // @ts-ignore
+      gtag("config", GA_TRACKING_ID, {
+        page_path: window.location.pathname,
+      });
+
+      // Track page views on route changes
+      const handleRouteChange = (url: string) => {
+        // @ts-ignore
+        gtag("config", GA_TRACKING_ID, {
+          page_path: url,
+        });
+      };
+
+      // Observe route changes
+      const { pathname } = window.location;
+      window.addEventListener("popstate", () => handleRouteChange(pathname));
+
+      return () => {
+        window.removeEventListener("popstate", () =>
+          handleRouteChange(pathname),
+        );
+      };
+    }
+  }, []);
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`dark:bg-black ${inter.className}`}>
@@ -38,6 +80,20 @@ export default function RootLayout({
           <Lines />
           <Header />
           <ToasterContext />
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_TRACKING_ID}', {
+            page_path: window.location.pathname,
+          });
+        `}
+          </Script>
           {children}
           <Footer />
           <ScrollToTop />
